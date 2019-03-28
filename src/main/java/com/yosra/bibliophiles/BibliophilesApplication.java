ace3b378970e189e4f1b8d652b73d20a1e6277ca
YosraRiabi <riabiyosraa@gmail.com>
2019-03-28 14:36:35 +0100

    Initial Commit

---
+++ src/main/java/com/yosra/bibliophiles/BibliophilesApplication.java
@@ -0,0 +1,13 @@
+package com.yosra.bibliophiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliophilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliophilesApplication.class, args);
+    }
+
+}
