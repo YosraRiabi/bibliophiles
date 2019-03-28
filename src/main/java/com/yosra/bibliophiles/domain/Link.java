46390efcc84862358a414ceaa85e9ee2017d99e2
YosraRiabi <riabiyosraa@gmail.com>
2019-03-28 15:00:12 +0100

    Created entities & refactored using Project Lombok

---
+++ src/main/java/com/yosra/bibliophiles/domain/Link.java
@@ -0,0 +1,19 @@
+package com.yosra.bibliophiles.domain;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String url;
+// comments
+}
