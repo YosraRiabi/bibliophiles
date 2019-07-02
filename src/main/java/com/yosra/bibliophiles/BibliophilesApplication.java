5e3d6098e5602ded4c09c0649f78abf0532d21ed
YosraRiabi <riabiyosraa@gmail.com>
2019-07-02 14:53:09 +0200

    Added account register and profile mappings and templates

--- src/main/java/com/yosra/bibliophiles/BibliophilesApplication.java
+++ src/main/java/com/yosra/bibliophiles/BibliophilesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 
 @SpringBootApplication
@@ -22,6 +23,14 @@ public class BibliophilesApplication {
     @Bean
     PrettyTime prettyTime(){
         return new PrettyTime();
+    }
+
+
+    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+// TODO for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extrasspringsecurity4)
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
     }
 
 }
